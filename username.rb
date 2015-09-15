dac906b39a9724367cb9cf752e7ff9e31217d7f0
liv123 <olivia.e@gmail.com>
2015-09-15 16:17:54 -0700

    solved thru format_year

--- username.rb
+++ username.rb
@@ -2,17 +2,24 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  first = first.gsub(/ /, "").downcase
-  last = last.gsub(/ /, "").downcase
+  first = first.gsub(/[\W\d]/, "").downcase
+  last = last.gsub(/[\W\d]/, "").downcase
 	if (!first.empty? && !last.empty?) 
-  	first[0] + last
+  	name = first[0] + last
   else
-  	nil
+  	name = nil
   end
+  name
 end
 
-def format_year
-  nil
+def format_year(year)
+	
+	puts year.to_s[-2..-1]
+	year.to_s.length === 4 ? year.to_s[-2..-1] : nil
+end
+
+def check_privilege(i = 0)
+
 end
 
 def build_username
